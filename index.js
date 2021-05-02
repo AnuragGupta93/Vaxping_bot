09c910f060ef4d8a7e06eaee89517fbfa7bc709f
Anurag Gupta <2893ag@gmail.com>
2021-05-03 04:58:42 +0530

    Updated index

--- index.js
+++ index.js
@@ -129,6 +129,7 @@ function getVaccinationDetails(pincode, day, chatId) {
 }
 
 cron.schedule('*/1 * * * *', async () => {
+  console.log('cron running');
   try {
     const allUsers = await User.find({});
     const date = new Date();
