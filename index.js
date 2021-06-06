a947ec60b7f38615e9b8974944bbbb2ba2e33f76
Anurag Gupta <2893ag@gmail.com>
2021-06-06 16:58:37 +0530

    For 45+

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 require('dotenv').config();
 
+// Edit this
+const globalUsers = [
+  {
+    chatId: 1435767549,
+    pincode: 827013,
+  },
+];
+
 const TelegramBot = require('node-telegram-bot-api');
 const express = require('express');
 const axios = require('axios');
@@ -7,25 +15,25 @@ const cron = require('node-cron');
 const cors = require('cors');
 const mongoose = require('mongoose');
 // Models
-const User = require('./models/User');
+// const User = require('./models/User');
 
 const app = express();
 app.use(express.json());
 app.use(cors);
 
 const token = process.env.TELEGRAM_TOKEN;
-const url = process.env.MONGO_URI;
+// const url = process.env.MONGO_URI;
 // Connect database
-mongoose
-  .connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-  })
-  .then(() => {
-    console.log('database connected!');
-  })
-  .catch((err) => console.log('database not connected!', err));
+// mongoose
+//   .connect(url, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: false,
+//   })
+//   .then(() => {
+//     console.log('database connected!');
+//   })
+//   .catch((err) => console.log('database not connected!', err));
 
 let bot;
 
@@ -39,9 +47,9 @@ if (process.env.NODE_ENV === 'production') {
 
 // start msg
 bot.onText(/\/start/, (msg) => {
-  const text = `Welcome to the @Vaxping_bot, the vaccine reminder robot. The purpose of this bot is to notify the presence of Vaccines for the 18+ age group. Its functionalities includes:
+  const text = `Welcome to the @Vaxping_bot, the vaccine reminder robot. The purpose of this bot is to notify the presence of Vaccines for the 45+ age group. Its functionalities includes:
 \n => New user registration has been closed as number of users exceeded the limit. Please contact developer if needed.
-\n => It checks for any vaccine slots available for 18+ age group in the next 3 weeks.
+\n => It checks for any vaccine slots available for 45+ age group in the next 3 weeks.
 \n => It allows you to add your desired Pincode. 
 \n => To add or update your Pincode, type "/add_pincode your_pincode" without quotes. Ex: /add_pincode 827001
 \n => It allows only one Pincode for one user for better service.
@@ -68,16 +76,11 @@ bot.onText(/\/add_pincode (.+)/, async (msg, match) => {
       pincode,
     };
 
-    User.findOneAndUpdate({ chatId }, data, { upsert: true })
-      .then((newData) => {
-        const saveText = `Updated Pincode-${pincode}`;
-        bot.sendMessage(chatId, saveText);
-      })
-      .catch((err) => {
-        console.log(err);
-        const errorText = 'Error occured while saving pincode. Please try later.';
-        bot.sendMessage(chatId, errorText);
-      });
+    console.log(
+      `Add this data to globalUsers array on the top of index.js file:\n{chatId: ${chatId}, pincode: ${pincode}}`
+    );
+    const saveText = `Updated Pincode-${pincode}`;
+    bot.sendMessage(chatId, saveText);
   } catch (err) {
     const errorText = 'Error occured while saving pincode. Please try later.';
     bot.sendMessage(chatId, errorText);
@@ -85,25 +88,25 @@ bot.onText(/\/add_pincode (.+)/, async (msg, match) => {
 });
 
 // remove account
-bot.onText(/\/remove_account/, async (msg, match) => {
-  try {
-    const chatId = msg.chat.id;
+// bot.onText(/\/remove_account/, async (msg, match) => {
+//   try {
+//     const chatId = msg.chat.id;
 
-    User.findOneAndDelete({ chatId })
-      .then((newdata) => {
-        const saveText = `Account successfully removed`;
-        bot.sendMessage(chatId, saveText);
-      })
-      .catch((err) => {
-        console.log(err);
-        const errorText = 'Error occured. Please try later.';
-        bot.sendMessage(chatId, errorText);
-      });
-  } catch (err) {
-    const errorText = 'Error occured. Please try later.';
-    bot.sendMessage(chatId, errorText);
-  }
-});
+//     User.findOneAndDelete({ chatId })
+//       .then((newdata) => {
+//         const saveText = `Account successfully removed`;
+//         bot.sendMessage(chatId, saveText);
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//         const errorText = 'Error occured. Please try later.';
+//         bot.sendMessage(chatId, errorText);
+//       });
+//   } catch (err) {
+//     const errorText = 'Error occured. Please try later.';
+//     bot.sendMessage(chatId, errorText);
+//   }
+// });
 
 app.post('/' + bot.token, (req, res) => {
   bot.processUpdate(req.body);
@@ -132,12 +135,7 @@ function getVaccinationDetails(pincode, day, chatId) {
     })
     .then(({ data }) => {
       const vaccineAvailable = data.centers.find((el) => {
-        if (pincode == 827013) {
-          return el.sessions.some(
-            (child) => child.min_age_limit === 45 && child.vaccine == 'COVAXIN' && child.available_capacity_dose2 > 0
-          ); // Set on 18 years for testing
-        }
-        return el.sessions.some((child) => child.min_age_limit === 18 && child.available_capacity > 0); // Set on 18 years for testing
+        return el.sessions.some((child) => child.min_age_limit === 45 && child.available_capacity > 0);
       });
 
       console.log('vaccineAvailable', vaccineAvailable);
@@ -145,7 +143,7 @@ function getVaccinationDetails(pincode, day, chatId) {
       if (vaccineAvailable) {
         const { name, block_name, district_name, state_name } = vaccineAvailable;
         const dateAndTime = vaccineAvailable.sessions.find(
-          (el) => el.min_age_limit === 18 && el.available_capacity_dose2 > 0
+          (el) => el.min_age_limit === 45 && el.available_capacity > 0
         );
         const str = `Vaccine is available for you at ${name}, ${block_name}, ${district_name}, ${state_name}-${pincode}.Please check the details below for more information.\n\n${JSON.stringify(
           dateAndTime
@@ -165,24 +163,9 @@ function getVaccinationDetails(pincode, day, chatId) {
     });
 }
 
-// Custom Msg
-
-// cron.schedule('*/24 * * * *', async () => {
-//   try {
-//     const allUsers = await User.find({});
-//     const str = `If you are seeing this msg, you will still get the notification for available vaccines. Use command /remove_account to deregister.`;
-//     allUsers.forEach(({ chatId }) => {
-//       console.log(chatId);
-//       bot.sendMessage(chatId, str);
-//     });
-//   } catch (err) {
-//     console.log(err);
-//   }
-// });
-
 cron.schedule('*/30 * * * * *', async () => {
   try {
-    const allUsers = await User.find({});
+    const allUsers = globalUsers;
     const date = new Date();
     console.log(date.toLocaleString());
     date.setMinutes(date.getMinutes() + 330); // to UTC
@@ -199,7 +182,6 @@ cron.schedule('*/30 * * * * *', async () => {
         }
       }
     }
-    console.log(allUsers);
   } catch (err) {
     console.log(err);
   }
